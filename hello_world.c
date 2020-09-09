d7f47457bb5ae239f9119e626b3dd39a569ee112
leventenyiro <nyiro.levente@gmail.com>
2020-09-09 17:07:26 +0200

    first commit

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main() {
+        printf("Hello World!\n");
+
+        if (3 > 1) {
+                printf("ok\n");
+        } else if (2 == 4) {
+                printf("majdnem ok\n");
+        } else {
+                printf("nem ok\n");
+        }
+}
